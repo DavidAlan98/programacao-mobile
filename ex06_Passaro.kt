5f58917317c7d026156014e4db110f3a7f110c86
DavidAlan98 <100147801+DavidAlan98@users.noreply.github.com>
2024-09-27 20:44:51 -0300

    Delete ex06_Passaro.kt

--- ex06_Passaro.kt
+++
@@ -1,8 +0,0 @@
-// Define a classe Passaro que implementa a interface Voador
-class Passaro(val nome: String) : Voador {
-
-    // Implementa o método voar para pássaros
-    override fun voar() {
-        println("$nome está voando como um pássaro.")  // Exibe a ação de voar
-    }
-}
