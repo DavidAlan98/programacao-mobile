e738f8f976054616788583889c1da5de65395b4c
DavidAlan98 <100147801+DavidAlan98@users.noreply.github.com>
2024-09-27 20:42:51 -0300

    Add files via upload

--- prova01/Exercicio 08/ex08.kt
+++ prova01/Exercicio 08/ex08.kt
@@ -1,1 +1,13 @@
+fun main() {
+    // Solicita um número inteiro ao usuário
+    print("Digite um número inteiro: ")
 
+    try {
+        // Tenta converter a entrada para um número inteiro
+        val numero = readLine()?.toInt()
+        println("Número digitado: $numero")  // Exibe o número digitado
+    } catch (e: NumberFormatException) {
+        // Captura a exceção se a entrada não for um número inteiro
+        println("Valor digitado é inválido. Por favor, insira um número inteiro.")
+    }
+}
