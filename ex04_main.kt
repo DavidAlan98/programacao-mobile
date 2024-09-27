c43b631811eeee47599004541edefc18d7328fa1
DavidAlan98 <100147801+DavidAlan98@users.noreply.github.com>
2024-09-27 20:45:37 -0300

    Delete ex04_main.kt

--- ex04_main.kt
+++
@@ -1,7 +0,0 @@
-fun main() {
-    // Cria um objeto Pessoa e inicializa seus atributos
-    val pessoa = Pessoa("João", 30, "São Paulo")
-
-    // Chama o método para exibir as informações
-    pessoa.mostrarInformacoes()
-}
