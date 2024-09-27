82bf174d576f421b3bef0d34e66aecd6f6b7e723
DavidAlan98 <100147801+DavidAlan98@users.noreply.github.com>
2024-09-27 20:45:00 -0300

    Delete ex06_Helicoptero.kt

--- ex06_Helicoptero.kt
+++
@@ -1,8 +0,0 @@
-// Define a classe Helicoptero que implementa a interface Voador
-class Helicoptero(val modelo: String) : Voador {
-
-    // Implementa o método voar para helicópteros
-    override fun voar() {
-        println("O helicóptero $modelo está voando.")  // Exibe a ação de voar
-    }
-}
