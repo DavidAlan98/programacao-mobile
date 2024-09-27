83218f8b9ae526b542fdaf9c061e63265324ae9a
DavidAlan98 <100147801+DavidAlan98@users.noreply.github.com>
2024-09-27 20:45:52 -0300

    Delete ex04_Pessoa.kt

--- ex04_Pessoa.kt
+++
@@ -1,9 +0,0 @@
-class Pessoa(val nome: String, val idade: Int, val cidade: String) {
-
-    // Método para exibir todas as informações da pessoa
-    fun mostrarInformacoes() {
-        println("Nome: $nome")  // Exibe o nome
-        println("Idade: $idade")  // Exibe a idade
-        println("Cidade: $cidade")  // Exibe a cidade
-    }
-}
